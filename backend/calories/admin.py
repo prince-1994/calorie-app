53a31e53768cd5f49e0edd5823f8fc0e8c67f7af
Yuvraj Sahu <yuvrajsahu007@gmail.com>
2021-11-15 01:44:34 +0530

    implemented admin reporting ui for metrics on food-calorie entries

--- backend/calories/admin.py
+++ backend/calories/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from .models import FoodCalorie
+from django.utils import timezone
+from datetime import timedelta
 
-admin.site.register(FoodCalorie)
+class FoodCalorieAdmin(admin.ModelAdmin):
+    list_display = ('name', 'consumed_at', 'calorie', 'user', 'created_at')
+
+    def changelist_view(self, request, extra_context={}):
+        now = timezone.now()
+        delta = timedelta(days=7)
+        extra_context['no_of_entry_last_7_days'] = FoodCalorie.get_no_entries(now-delta, now)
+        extra_context['no_of_entry_last_to_last_7_days'] = FoodCalorie.get_no_entries(now-2*delta, now-delta)
+        extra_context['avg_calories_per_user_last_7_day'] = FoodCalorie.get_avg_calorie_per_user(now-delta, now)
+        print(extra_context)
+        return super().changelist_view(request, extra_context=extra_context)
+    
+
+admin.site.register(FoodCalorie, FoodCalorieAdmin)
--- backend/calories/models.py
+++ backend/calories/models.py
@@ -8,6 +8,20 @@ class FoodCalorie(models.Model):
     calorie = models.PositiveIntegerField()
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     is_inactive = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def get_no_entries(cls, start, end):
+        return cls.objects.filter(created_at__gte = start, created_at__lte = end).count()
+
+    @classmethod
+    def get_avg_calorie_per_user(cls, start, end):
+        all_entries = cls.objects.filter(created_at__gte = start, created_at__lt = end)
+        distinct_users_count = all_entries.values_list('user', flat=True).distinct().count()
+        if distinct_users_count == 0:
+            return float('NaN')
+        total = all_entries.aggregate(models.Sum('calorie'))['calorie__sum']
+        return total/distinct_users_count
 
     def __str__(self):
         return self.name
