fc483be1e141fa5acd57668f14ccf560ccfab33a
Yuvraj Sahu <yuvrajsahu007@gmail.com>
2021-11-15 13:09:05 +0530

    added nested data for user profile

--- backend/userprofiles/serializers.py
+++ backend/userprofiles/serializers.py
@@ -1,7 +1,17 @@
+from django.db import models
+from django.db.models import fields
 from rest_framework import serializers
 from .models import UserProfile
+from django.contrib.auth import get_user_model
+
+class UserSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = get_user_model()
+            fields = ('username',)
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = UserProfile
-        exclude = ['user']
+        fields = '__all__'
